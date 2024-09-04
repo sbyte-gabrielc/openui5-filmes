173b95ce93f659a7e781c693bc1422a15bbc616f
sbyte-gabrielc <gchagas.sinosbyte@gmail.com>
2024-09-04 17:39:17 -0300

    Ultima correção

--- api_filmes/webapp/controller/Main.controller.js
+++ api_filmes/webapp/controller/Main.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("apifilmes.controller.Main", {
