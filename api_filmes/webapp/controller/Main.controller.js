a3745a2d854018fc549a696620500529f4352f2d
sbyte-gabrielc <gchagas.sinosbyte@gmail.com>
2024-09-04 16:58:07 -0300

    API CONNECTION

--- api_filmes/webapp/controller/Main.controller.js
+++ api_filmes/webapp/controller/Main.controller.js
@@ -9,7 +9,70 @@ sap.ui.define([
 
         return Controller.extend("apifilmes.controller.Main", {
             onInit: function () {
+                var oPopulares = new JSONModel();
+                var oAvaliados = new JSONModel();
+                var oBreve = new JSONModel();
+                this.getView().setModel(oPopulares, "Populares");
+                this.getView().setModel(oPopulares, "Avaliados");
+                this.getView().setModel(oPopulares, "Breve");
+                this.buscarPopulares();
+                this.buscarAvaliados();
+                this.buscarBreve();
+            },
 
-            }
+            buscarPopulares: function () {
+                var dadosModel = this.getView().getModel("Populares");
+
+                $.ajax({
+                    method: "GET",
+                    url: "https://api.themoviedb.org/3/tv/popular?language=pt-BR",
+                    headers: {
+                        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjE5ZjcxMGY3ZTVhY2RkZjA0MjBmYjdjNWU5MWEyOCIsIm5iZiI6MTcyNTQ3NjgxMi40NDk4MzgsInN1YiI6IjY2ZDhhZjExYjllOWEzODFlOTZjZWE0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CDPCeBnZ43nZ9-19CWnCoL-ITL01_a1ttXdXYOVayL4'
+                    },
+                    success: function (dados) {
+                        dadosModel.setData(dados);
+                    },
+                    error: function(erro) {
+                        console.log(erro);
+                    }
+                    
+                });
+            },
+            buscarAvaliados: function () {
+                var dadosModel = this.getView().getModel("Avaliados");
+
+                $.ajax({
+                    method: "GET",
+                    url: "https://api.themoviedb.org/3/movie/top_rated?language=pt-BR",
+                    headers: {
+                        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjE5ZjcxMGY3ZTVhY2RkZjA0MjBmYjdjNWU5MWEyOCIsIm5iZiI6MTcyNTQ3NjgxMi40NDk4MzgsInN1YiI6IjY2ZDhhZjExYjllOWEzODFlOTZjZWE0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CDPCeBnZ43nZ9-19CWnCoL-ITL01_a1ttXdXYOVayL4'
+                    },
+                    success: function (dados) {
+                        dadosModel.setData(dados);
+                    },
+                    error: function(erro) {
+                        console.log(erro);
+                    }
+
+                });
+            },
+            buscarBreve: function () {
+                var dadosModel = this.getView().getModel("Breve");
+
+                $.ajax({
+                    method: "GET",
+                    url: "https://api.themoviedb.org/3/movie/upcoming?language=pt-BR",
+                    headers: {
+                        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjE5ZjcxMGY3ZTVhY2RkZjA0MjBmYjdjNWU5MWEyOCIsIm5iZiI6MTcyNTQ3NjgxMi40NDk4MzgsInN1YiI6IjY2ZDhhZjExYjllOWEzODFlOTZjZWE0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CDPCeBnZ43nZ9-19CWnCoL-ITL01_a1ttXdXYOVayL4'
+                    },
+                    success: function (dados) {
+                        dadosModel.setData(dados);
+                    },
+                    error: function(erro) {
+                        console.log(erro);
+                    }
+
+                });
+            },
         });
     });
